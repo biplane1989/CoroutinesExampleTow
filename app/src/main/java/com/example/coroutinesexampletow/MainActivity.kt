a053c216ac4f96d680bd77291115cb20efcbe682
giangtd <68835891+tomatoesamerica@users.noreply.github.com>
2021-02-23 13:45:46 +0700

    update

--- app/src/main/java/com/example/coroutinesexampletow/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesexampletow/MainActivity.kt
@@ -4,26 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlin.Exception
 import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "giangtd"
-
+    lateinit var cancel: TextView
+    lateinit var start: TextView
     var job1: Job? = null
 
     var job2: Job = SupervisorJob()
     val myScop = CoroutineScope(Dispatchers.Main + job2)
 
+    fun foo(): Flow<Int> = flow {
+        println("Flow started")
+        for (i in 1..3) {
+            delay(100)
+            emit(i)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val cancel: TextView = findViewById(R.id.cancel)
-        val start: TextView = findViewById(R.id.start)
-
+        cancel = findViewById(R.id.cancel)
+        start = findViewById(R.id.start)
 
         start.setOnClickListener {
             lifecycleScope.launch {
@@ -32,7 +44,7 @@ class MainActivity : AppCompatActivity() {
                 job2 = scop2()
             }
         }
-        
+
         cancel.setOnClickListener {             // cancel 1 coroutines
 //            job1?.cancel()                     // cách 1
             job2.cancelChildren()               // cách 2
@@ -46,7 +58,7 @@ class MainActivity : AppCompatActivity() {
 //        }
 //        Thread.sleep(3000)
 //        Log.d(TAG, "onCreate: end")
-        
+
     }
 
     fun scop1() = lifecycleScope.launch(Dispatchers.Default) {      // scop của lifecycle
@@ -59,6 +71,15 @@ class MainActivity : AppCompatActivity() {
 
     fun scop2() = myScop.launch(Dispatchers.Default) {
         try {
+
+            val flow = foo()
+            flow.collect {
+                Log.d(TAG, "flow: "+ it)
+            }
+
+            withContext(Dispatchers.Main) {             // thay đổi sang luồng main để cập nhật ui
+                cancel.setText("orange")
+            }
             val a = withContext(Dispatchers.Default) { sumA() }
             val b = sumB()
             val c = async { sumC() }
@@ -103,4 +124,6 @@ class MainActivity : AppCompatActivity() {
         val result = a + b + c + d
         Log.d(TAG, "sum: a + b: " + result)
     }
+
+
 }
